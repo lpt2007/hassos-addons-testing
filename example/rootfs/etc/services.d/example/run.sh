0d2596d8159484685fd20604f9e29e3d42b31378
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2022-03-25 19:15:31 +0100

    Update run.sh

--- example/rootfs/etc/services.d/example/run.sh
+++ example/rootfs/etc/services.d/example/run.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Starting..."
+echo "Started..."
 v4l2rtspserver -W 1920 -H 1088 -F 15 -P 8554 /dev/video0
-
-### Wait script - waiting rtsp server to start
-x=10
-while [ $x -gt 0 ]
-do
-sleep 1s
-clear
-echo "$x seconds"
-x=$(( $x - 1 ))
-done
-echo "RTSP server started"
