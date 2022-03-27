83a2697ec594cc467105d41cf9117f60bf00d27b
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2022-03-27 16:10:04 +0200

    Update start.sh

--- kodi-smb-from-balena/data/addon/kodi-smb-from-balena/start.sh
+++ kodi-smb-from-balena/data/addon/kodi-smb-from-balena/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #pHAT DAC (removes default audio)
-if [[ "$PHAT_DAC" = "1" ]]; then
-  rmmod snd_bcm2835 || true
-fi
+###if [[ "$PHAT_DAC" = "1" ]]; then
+###  rmmod snd_bcm2835 || true
+###fi
 
 # Handle Kodi userdata folder
 mkdir /data/kodi >/dev/null 2>&1 || true && rm -rf /root/.kodi && ln -s /data/kodi /root/.kodi
