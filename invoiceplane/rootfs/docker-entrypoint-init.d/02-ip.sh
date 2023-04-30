e2760ef5fc6285c37db8e4d8dbd37add834acd09
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2023-04-30 21:53:11 +0200

    Update 02-ip.sh

--- invoiceplane/rootfs/docker-entrypoint-init.d/02-ip.sh
+++ invoiceplane/rootfs/docker-entrypoint-init.d/02-ip.sh
@@ -1,45 +1,54 @@
 #!/bin/sh
 #DocumentRoot=$(bashio::config 'document_root')
 
-# Preveri, ali je mapa /share/invoiceplane že nameščena
-if [ -f "/var/www/html/ipconfig.php" ]; then
-  echo "InvoicePlane je že nameščen."
-#  exit 0
+if [ -L /var/www/html ]; then
+  if [ -f /var/www/html/ipconfig.php ]; then
+    echo "Simbolična povezava /var/www/html obstaja in datoteka ipconfig.php je prisotna."
+  else
+    echo "Simbolična povezava /var/www/html obstaja, vendar datoteka ipconfig.php ni prisotna."
+  fi
 else
+  echo "Simbolična povezava /var/www/html ne obstaja."
   echo "InvoicePlane ni nameščen."
   echo "Nameščam..."
+  
   # Preveri, ali je dosegljiva povezava do GitHuba
+  echo "Datoteka je prisotna na GITHUB-u...ok"
   if ! curl --output /dev/null --silent --head --fail "https://github.com/InvoicePlane/InvoicePlane/releases/download/v1.6.1-beta-2/v1.6.1-beta-2.zip"; then
     echo "Ni mogoče prenesti InvoicePlane. Povezava do GitHuba ni na voljo."
     exit 1
   fi
 
   # Prenesi InvoicePlane
+  echo "InvoicePlane se prenaša iz GITHUB-a...ok"
   curl -LJO https://github.com/InvoicePlane/InvoicePlane/releases/download/v1.6.1-beta-2/v1.6.1-beta-2.zip
 
-  # Razpakiraj arhiv v mapo /var/www/html/
-  unzip v1.6.1-beta-2.zip -d /var/www/html
+  # Razpakiraj arhiv v mapo /share/invoiceplane
+  echo "Dearhiviranje datoteke se je pričel...ok"
+  unzip v1.6.1-beta-2.zip -d /share/invoiceplane
   # Odstrani preneseni arhiv
+  echo "Arhivska datoteka bo odstranjena...ok"
   rm -f v1.6.1-beta-2.zip
   # prestavim vsebino iz ip mape v /var/www/html
-  mv /var/www/html/ip/* /var/www/html/
+  mv /share/invoiceplane/ip/* /share/invoiceplane
   # zbrišem prazno mapo ip
-  rm -r /var/www/html/ip
+  rm -r /share/invoiceplane/ip
   # kopira datoteko ipconfig.php.example v ipconfig.php
-  cp /var/www/html/ipconfig.php.example /var/www/html/ipconfig.php
+  cp /share/invoiceplane/ipconfig.php.example /share/invoiceplane/ipconfig.php
+  # ustvari simbolični link med /share/invoiceplane in /var/www/
+  ln -s /share/invoiceplane /var/www/
 
-  # Sporoči, da je bila namestitev InvoicePlane uspešna
-  echo "InvoicePlane je bil uspešno nameščen v /var/www/html/."
   
-  #if [ -L "/var/www/html" ]; then
-  #  echo "Povezava med /share/invoiceplane/ip/ in /var/www/html je že vspostavljena"
-  #  exit 1
-  #else
-  #  echo "Povezava med /share/invoiceplane/ip/ in /var/www/html še ni vspostavljena"
-  #  echo "Vzpostavljam..."
-  #  ln -s /share/invoiceplane/ip/* /var/www/html/
-  #  echo "Mapa uspešno povezana na strežnik"
-  #fi
+    # Preveri, ali je mapa /var/www/invoiceplane pravilno viden
+    if [ -f "/var/www/html/ipconfig.php" ]; then
+    # Sporoči, da je bila namestitev InvoicePlane uspešna
+    echo "InvoicePlane je bil uspešno nameščen v /var/www/invoiceplane/."
+    else
+    echo "NAPAKA"
+    exit 1
+  
 
+
+    fi
+  
 fi
-#exit 1
